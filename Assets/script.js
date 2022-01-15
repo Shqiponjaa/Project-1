4100a3706116db0e1787ad652718288e68137f2f
Shqiponjaa <94502514+Shqiponjaa@users.noreply.github.com>
2022-01-15 18:05:55 -0500

    commit

--- Assets/script.js
+++ Assets/script.js
@@ -1,11 +1,22 @@
-var saveBtn = $(".saveBtn");
 
-saveBtn.on("click", function () {
-    var time = $(this).parent().attr("id");
-    var schedule = $(this).siblings(".schedule").val();
-    localStorage.setItem(time, schedule);
+$.datepicker.parseDate( "yy-mm-dd", "2007-01-26" );
+
+
+
+  $(function() {
+    $("#datepicker").datepicker();
+    $("#datepicker1").datepicker();
 });
 
+
+  function convertTxtToDate() {
+    $('.dateTxt').each(function () {
+        if ($(this).hasClass('hasDatepicker')) {
+            $(this).removeClass('hasDatepicker');
+        } 
+         $(this).datepicker();
+    });
+} 
 // closes the panel on click outside
 $(document).mouseup(function (e) {
     var container = $('#contact-panel');
@@ -16,4 +27,19 @@ $(document).mouseup(function (e) {
       }
   });
   
+  var dt = new Date();
+  document.getElementById("datetime")
+  .innerHTML = (("0"+dt.getDate()).slice(-2)) +"."+ (("0"+(dt.getMonth()+1)).slice(-2)) +"."+ (dt.getFullYear()) +" "+ (("0"+dt.getHours()).slice(-2)) +":"+ (("0"+dt.getMinutes()).slice(-2));
+
+
+
+var textToWrite = document.getElementById("primary button expanded search-button").value;
+var textFileAsBlob = new Blob([textToWrite], {type:'text/plain'});
+
+var downloadLink = document.createElement("a");
+downloadLink.download = "log.txt";
+downloadLink.innerHTML = "Download File";
+
+downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
+downloadLink.click();
   
