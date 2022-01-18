ba6e2d66e9f1f177b0e0a348a09ab4c2011c7b2f
Titus Knox <knoxtitus@gmail.com>
2022-01-18 10:18:49 -0500

    added necessary fetch requests for car rental page

--- script.js
+++ script.js
@@ -1,40 +1,49 @@
-let ul = $("#searchResults");
-let fromBox = $("#carFrom");
-let toBox = $("#carTo");
-let startBox = $("#carStart");
-let returnBox = $("#carReturn");
-let searchBtn = $("#searchBtn");
-
-// searchBtn.onclick = function() {
-//     fetch("https://booking-com.p.rapidapi.com/v1/car-rental/search?pick_up_datetime=" + startBox + "%2013%3A00%3A00&pick_up_longitude=" + fromBox + "&drop_off_longitude=" + toBox + "&pick_up_latitude=" 55.7518540820001 &drop_off_latitude=55.7518540820001&sort_by=recommended&locale=en-gb&currency=AED&drop_off_datetime=2022-07-02%2013%3A00%3A00&from_country=it")
-// }
+$(document).ready(function(){
+    let rentalInputBox = $("#rentalInputBox");
+    let rentalSearchBtn = $("#rentalSearchBtn");
+    let rentalPickupBox = $("#rentalPickupDate");
+    let rentalDropoffBox = $("#rentalDropoffDate");
 
 
-fetch("https://booking-com.p.rapidapi.com/v1/car-rental/search?pick_up_datetime=2022-07-01%2013%3A00%3A00&pick_up_longitude=37.620230899&drop_off_longitude=37.620230899&pick_up_latitude=55.7518540820001&drop_off_latitude=55.7518540820001&sort_by=recommended&locale=en-gb&currency=AED&drop_off_datetime=2022-07-02%2013%3A00%3A00&from_country=it", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "booking-com.p.rapidapi.com",
-		"x-rapidapi-key": "c3e07b89cfmshe09e43ad009e017p10e676jsne90c7a2719f6",
-        "Content-Type" : "application/json"
-	}
+    rentalSearchBtn.click(function(){
+        let cityInput = rentalInputBox.val();
+        let pickupInput = rentalPickupBox.val();
+        let dropoffInput = rentalDropoffBox.val();
+
+        // fetch latitude and longitiude for searched city
+        fetch("https://booking-com.p.rapidapi.com/v1/car-rental/locations?name=" + cityInput + "&locale=en-us", {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "booking-com.p.rapidapi.com",
+            "x-rapidapi-key": "c3e07b89cfmshe09e43ad009e017p10e676jsne90c7a2719f6"
+        }
+        })
+        .then(response => response.json())
+        .then(function(coordinateResult){
+            let RentalLatitude = coordinateResult[0].latitude;
+            let rentalLongitude = coordinateResult[0].longitude;
+            let coordinates = [RentalLatitude, rentalLongitude]
+            
+            // fetch rental car data using the lat and lon with other input parameters
+            fetch("https://booking-com.p.rapidapi.com/v1/car-rental/search?pick_up_datetime=" + pickupInput + "%2013%3A00%3A00&pick_up_longitude=" + coordinates[1] + "&drop_off_longitude=" + coordinates[1] + "&pick_up_latitude=" + coordinates[0] + "&drop_off_latitude=" + coordinates[0] + "&sort_by=recommended&locale=en-us&currency=USD&drop_off_datetime=" + dropoffInput + "%2013%3A00%3A00&from_country=it", {
+                "method": "GET",
+                "headers": {
+                    "x-rapidapi-host": "booking-com.p.rapidapi.com",
+                    "x-rapidapi-key": "c3e07b89cfmshe09e43ad009e017p10e676jsne90c7a2719f6"
+                }
+            })
+            .then(response => response.json())
+            .then(function(rentalResults){
+                console.log(rentalResults);
+                console.log("fetch complete")
+            })
+            .catch(err => {
+                console.error(err);
+            });
+        })
+        .catch(err => {
+            console.error(err);
+        });
+        
+    })
 })
-.then(response => {
-	console.log(response)
-    return response.json();
-})
-.then(searchResult => {
-    console.log(JSON.stringify(searchResult));
-    // Display in HTML 
-    let carArray = searchResult.response;
-for(var i=0; i < carArray.length; i++){
-    var listItem = document.createElement("li");
-    listItem.innerHTML = carArray[i].vehicle_info;
-    ul.appendChild(listItem);
-}
-
-
-})
-.catch(err => {
-	    console.error(err);
-});
-console.log(ul);
