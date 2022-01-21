8623768ee4fd1360bda6888809970b6e5b12bf98
Shqiponjaa <94502514+Shqiponjaa@users.noreply.github.com>
2022-01-21 20:32:38 +0100

    commit

--- Assets/script.js
+++ Assets/script.js
@@ -4,21 +4,48 @@ $.datepicker.parseDate("yy-mm-dd", "2007-01-26");
 $(function () {
     $("#datepicker").datepicker();
     $("#datepicker1").datepicker();
+    $("#datepicker2").datepicker();
+    $("#datepicker3").datepicker();
+    $("#datepicker4").datepicker();
+    $("#datepicker5").datepicker();
+    $("#datepicker6").datepicker();
+    $("#datepicker7").datepicker();
 });
 
 
+}
 
-
-$("#clearButton").click(function() {
-  $("#textbox1").val("");
+fetch("https://travelpayouts-travelpayouts-flight-data-v1.p.rapidapi.com/data/en-GB/airlines.json", {
+	"method": "GET",
+	"headers": {
+		"x-access-token": "undefined",
+		"x-rapidapi-host": "travelpayouts-travelpayouts-flight-data-v1.p.rapidapi.com",
+		"x-rapidapi-key": "ca68ba596fmshdaee9639acc60eap148251jsn32f8dfbe69d0"
+	}
+})
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.error(err);
 });
 
-window.onload =function(){ //ensures the page is loaded before functions are executed.
-    document.getElementById("'From','Return Date','To','Start Date'").onsubmit = store
-    document.getElementById("clearButton").onclick = clearStorage
-    document.getElementById("retrieveButton").onclick = retrieveRecords
+e.preventDefault();
+var search = searchInput.value.trim();
+fetchCoords(search);
+searchInput.value = '';
 }
-function populateStorage() {
-localStorage.setItem('From','Return Date','To','Start Date');
-  localStorage.clear();
+
+function handleSearchHistoryClick(e) {
+if (!e.target.matches('.btn-history')) {
+  return;
 }
+
+var btn = e.target;
+var search = btn.getAttribute('data-search');
+fetchCoords(search);
+}
+
+initSearchHistory();
+searchForm.addEventListener('submit', handleSearchFormSubmit);
+searchHistoryContainer.addEventListener('click', handleSearchHistoryClick);
