867d32170f8a123d0e20d83a4396b8609816bd4a
Shqiponjaa <94502514+Shqiponjaa@users.noreply.github.com>
2022-01-21 01:42:03 +0100

    commit

--- Assets/script.js
+++ Assets/script.js
@@ -1,31 +1,24 @@
 
 $.datepicker.parseDate("yy-mm-dd", "2007-01-26");
 
-
-
 $(function () {
     $("#datepicker").datepicker();
     $("#datepicker1").datepicker();
 });
 
 
-// closes the panel on click outside
-$(document).mouseup(function (e) {
-    var container = $('#contact-panel');
-    if (!container.is(e.target) // if the target of the click isn't the container...
-        && container.has(e.target).length === 0) // ... nor a descendant of the container
-    {
-        container.removeClass('is-active');
-    }
+
+
+$("#clearButton").click(function() {
+  $("#textbox1").val("");
 });
-var textToWrite = document.getElementById("primary button expanded search-button").value;
-var textFileAsBlob = new Blob([textToWrite], { type: 'text/plain' });
 
-var downloadLink = document.createElement("a");
-downloadLink.download = "log.txt";
-downloadLink.innerHTML = "Download File";
-
-downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
-downloadLink.click();
-
-
+window.onload =function(){ //ensures the page is loaded before functions are executed.
+    document.getElementById("'From','Return Date','To','Start Date'").onsubmit = store
+    document.getElementById("clearButton").onclick = clearStorage
+    document.getElementById("retrieveButton").onclick = retrieveRecords
+}
+function populateStorage() {
+localStorage.setItem('From','Return Date','To','Start Date');
+  localStorage.clear();
+}
